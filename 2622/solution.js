7b8fcd9add2bda1e2e199a491629e14a4b0e5bef
Vojtech Vachal <vachal.vojta@email.cz>
2024-06-19 18:08:14 +0200

    Solved some problems in 30 Days of JavaScript

---
+++ 2622/solution.js
@@ -0,0 +1,51 @@
+var TimeLimitedCache = function() {
+    this.cache = {}
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let res = false
+
+    const timeout = setTimeout(() => {
+        delete this.cache[key]
+    }, duration)
+
+    if(this.cache[key]) {
+        clearTimeout(this.cache[key].timeout)
+        res = true
+    }
+
+    this.cache[key] = {
+        value,
+        timeout
+    }
+
+    return res
+};
+
+/**
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache[key] ? this.cache[key].value : -1
+};
+
+/**
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return Object.keys(this.cache).length
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
